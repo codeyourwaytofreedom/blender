0ebafb0256380c2c949f1f7443a8f6f94d200314
codeyourwaytofreedom <can.kolej@gmail.com>
2023-03-20 14:02:48 +0300

    blender starts

---
+++ components/home.tsx
@@ -0,0 +1,11 @@
+import h from "../styles/home.module.css";
+
+const Home_page = () => {
+    return ( 
+        <div className={h.home}>
+            Home Page
+        </div>
+      );
+}
+ 
+export default Home_page;
