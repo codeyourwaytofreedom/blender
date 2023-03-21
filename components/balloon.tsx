fe80654220791887c3dd50253969b1421de76ed6
codeyourwaytofreedom <can.kolej@gmail.com>
2023-03-22 00:10:29 +0300

    Amazing onclick method to trim svg objects.
    Will use this tomorrow for more object design.
    
    Keep going.
    
    Remember why you started.

--- components/balloon.tsx
+++ components/balloon.tsx
@@ -4,25 +4,32 @@ import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
 const loader = new SVGLoader();
 import {RepeatWrapping } from 'three';
 import { Vector3 } from "three";
+import { useState } from "react";
 
 const blocks:Object[] = [];
 const sscale:Vector3 = new Vector3(0.005, 0.005, 0.005);
+/* const included = [1,3, 5, 29, 27, 51, 47, 83, 85, 55, 53, 49, 
+    45, 79, 87, 81, 77, 57, 59, 7, 9, 25, 13, 11, 17, 15, 21, 
+    23, 19, 61, 63, 67, 65, 33, 37, 69, 75, 43, 35, 73, 71, 39, 41] */
 
+
+    const basic_but = [5, 7, 1, 3];
+    
 loader.load(
 	// resource URL
-	'/btf3.svg',
+	'/btf4.svg',
 	// called when the resource is loaded
 	function ( data ) {
 
 		const paths = data.paths;
-        console.log(paths[0].color)
+        //console.log(paths[0].color)
         for (let index = 0; index < paths.length; index++) {
             const element = paths[index];
-            console.log(element.color);
-            console.log(index)
+            //console.log(element.color);
+            //console.log(index)
 
             const s:Shape[] = SVGLoader.createShapes(element);
-            console.log(s)
+            //console.log(s)
             blocks.push(s[0])
             const ob = {
                 color:element.color,
@@ -35,11 +42,13 @@ loader.load(
 );
 
 const extrudeSettings = { depth: 2, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
-
+const remaining = [];
 const Balloon = () => {
     //const texture = useTexture("but.jpg");
     let texture;
     let col;
+    const [excluded, setExcluded] = useState([33, 37, 35,39, 43, 41,9,11,13, 15, 17, 19, 21,
+        31, 29, 27, 25, 23,73, 75, 71, 69,45, 47, 49, 51, 61, 63,53, 67, 65, 55, 59, 57]);
 /*     texture.wrapS = RepeatWrapping;
     texture.wrapT = RepeatWrapping;
     texture.repeat.set( 0.001, 0.001 ); */
@@ -47,25 +56,21 @@ const Balloon = () => {
         <>
         <group position={[0,1,0]} scale={4}>
             {
-                blocks.slice(2,blocks.length-1).map ((b,i) =>
-                <mesh scale={sscale} rotation={[0,0,Math.PI]} onClick={()=>console.log(5555)} >
-                    <extrudeBufferGeometry args={[b.shp, extrudeSettings]}/>            
-                    <meshBasicMaterial map={texture??null} color={b.color}/>
+                blocks.map ((b,i) => !excluded.includes(i) &&
+                <mesh scale={sscale} key={i} rotation={[0,0,Math.PI]} 
+                    onClick={(e)=>{
+                    e.stopPropagation();
+                    console.log(i); 
+                    setExcluded([...excluded, i]);
+                    remaining.push(i);
+                    console.log(remaining)
+                    }} 
+                >
+                    <extrudeGeometry args={[b.shp, extrudeSettings]}/>            
+                    <meshBasicMaterial map={texture??null} color={b.color} />
                 </mesh> 
                 )
             }
-{/*         <mesh scale={sscale} rotation={[0,0,Math.PI]} >
-            <extrudeBufferGeometry args={[blocks[0], extrudeSettings]}/>            
-            <meshBasicMaterial map={texture??null} color={col??"crimson"}/>
-        </mesh> 
-        <mesh scale={sscale} rotation={[0,0,Math.PI]} >
-            <extrudeBufferGeometry args={[blocks[1], extrudeSettings]}/>            
-            <meshBasicMaterial map={texture??null} color={col??"crimson"}/>
-        </mesh> 
-        <mesh scale={sscale} rotation={[0,0,Math.PI]} >
-            <extrudeBufferGeometry args={[blocks[2], extrudeSettings]}/>            
-            <meshBasicMaterial map={texture??null} color={col??"crimson"}/>
-        </mesh>  */}
         </group>
 
         </>
