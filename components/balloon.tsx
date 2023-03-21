1ba4409075480b362e41937e29deb9fc96776d97
codeyourwaytofreedom <can.kolej@gmail.com>
2023-03-21 15:16:45 +0300

    Another milestone in 3D experimenting.
    SVG extruding achieved.
    
    Great step forward.
    
    Keep going.
    
    Remember why you started.

--- components/balloon.tsx
+++ components/balloon.tsx
@@ -4,24 +4,29 @@ import { DoubleSide, ExtrudeGeometry, Line3 } from "three";
 import arrow from "../public/arrow.svg";
 import { Vector2 } from "@react-three/fiber";
 import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
-
+import { useEffect, useState } from "react";
+import { Text } from "@react-three/drei";
 const loader = new SVGLoader();
-const shapes = [];
+const shps = [];
+
+let test;
 loader.load(
 	// resource URL
-	'/arrow.svg',
+	'/btf.svg',
 	// called when the resource is loaded
 	function ( data ) {
 
 		const paths = data.paths;
-        console.log(paths)
+        console.log(paths.length)
+        const s = SVGLoader.createShapes(data.paths[1]);
+        test = s;
 
-        for ( let i = 0; i < paths.length; i ++ ) {
+/*         for ( let i = 0; i < paths.length; i ++ ) {
 
 			const path = paths[ i ];
             //react equivalent
+            const p = [];
             const mat = <meshBasicMaterial side={DoubleSide} depthWrite={false} color={path.color} />
-
 			const material = new THREE.MeshBasicMaterial( {
 				color: path.color,
 				side: THREE.DoubleSide,
@@ -32,12 +37,14 @@ loader.load(
 
 			for ( let j = 0; j < shapes.length; j ++ ) {
 
-				const shape = shapes[ j ];
-				const geometry = new THREE.ShapeGeometry( shape );
-				const mesh = new THREE.Mesh( geometry, material );
+                const mmesh = 
+                            <mesh>
+                                <shapeGeometry args={[shapes[ j ]]}/>
+                                {mat}
+                            </mesh>
+                p.push(shapes[ j ])                
 			}
-
-		}
+		} */
 	},
     
 	// called when loading is in progresses
@@ -55,24 +62,20 @@ loader.load(
 );
 
 const extrudeSettings = { depth: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
-const x = 0;
-const y = 0;
-const fishShape = new THREE.Shape()
-					.moveTo( x, y )
-					.quadraticCurveTo( x + 50, y - 80, x + 90, y - 10 )
-					.quadraticCurveTo( x + 100, y - 10, x + 115, y - 40 )
-					.quadraticCurveTo( x + 115, y, x + 115, y + 40 )
-					.quadraticCurveTo( x + 100, y + 10, x + 90, y + 10 )
-					.quadraticCurveTo( x + 50, y + 80, x, y );
+
 
 const Balloon = () => {
-    const texture = useTexture("book.jpg")
+    const texture = useTexture("book.jpg");
+    const [meshes, setMeshes] = useState([]);
+    useEffect(()=>{
+        shps.map(s=> console.log(s))
+    },[])
     return ( 
         <>
-        <mesh scale={0.02} position={[1,0,0]}>
-            <extrudeBufferGeometry args={[fishShape, extrudeSettings]}/>            
-            <meshBasicMaterial attach="material" color="black" transparent={true} opacity={0.8} side={DoubleSide} />
-        </mesh>
+        <mesh scale={0.001} position={[0,0,0]}>
+            <extrudeBufferGeometry args={[test, extrudeSettings]}/>            
+            <meshBasicMaterial color={"darkorange"} side={DoubleSide} />
+        </mesh> 
         </>
      );
 }
