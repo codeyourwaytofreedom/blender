6569ba7ee577afb2464a45c6a1ca41cca34d8fd2
codeyourwaytofreedom <can.kolej@gmail.com>
2023-03-21 23:11:01 +0300

    SVG path color applied to mesh.
    Looking great.
    
    Keep going.
    
    Remember why you started.

--- components/balloon.tsx
+++ components/balloon.tsx
@@ -3,34 +3,71 @@ import { Shape } from "three";
 import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
 const loader = new SVGLoader();
 import {RepeatWrapping } from 'three';
+import { Vector3 } from "three";
 
-let test:Shape[];
+const blocks:Object[] = [];
+const sscale:Vector3 = new Vector3(0.005, 0.005, 0.005);
+
 loader.load(
 	// resource URL
-	'/btf.svg',
+	'/btf3.svg',
 	// called when the resource is loaded
 	function ( data ) {
 
 		const paths = data.paths;
-        console.log(paths.length)
-        const s = SVGLoader.createShapes(data.paths[1]);
-        test = s;
+        console.log(paths[0].color)
+        for (let index = 0; index < paths.length; index++) {
+            const element = paths[index];
+            console.log(element.color);
+            console.log(index)
+
+            const s:Shape[] = SVGLoader.createShapes(element);
+            console.log(s)
+            blocks.push(s[0])
+            const ob = {
+                color:element.color,
+                shp:s[0]
+            }
+            blocks.push(ob)
+            
+        }
 	}
 );
 
-const extrudeSettings = { depth: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
+const extrudeSettings = { depth: 2, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
 
 const Balloon = () => {
-    const texture = useTexture("but.jpg");
-    texture.wrapS = RepeatWrapping;
+    //const texture = useTexture("but.jpg");
+    let texture;
+    let col;
+/*     texture.wrapS = RepeatWrapping;
     texture.wrapT = RepeatWrapping;
-    texture.repeat.set( 0.008, 0.008 );
+    texture.repeat.set( 0.001, 0.001 ); */
     return ( 
         <>
-        <mesh scale={0.005} rotation={[0,0,Math.PI]} >
-            <extrudeBufferGeometry args={[test, extrudeSettings]}/>            
-            <meshBasicMaterial map={texture}/>
+        <group position={[0,1,0]} scale={4}>
+            {
+                blocks.slice(2,blocks.length-1).map ((b,i) =>
+                <mesh scale={sscale} rotation={[0,0,Math.PI]} onClick={()=>console.log(5555)} >
+                    <extrudeBufferGeometry args={[b.shp, extrudeSettings]}/>            
+                    <meshBasicMaterial map={texture??null} color={b.color}/>
+                </mesh> 
+                )
+            }
+{/*         <mesh scale={sscale} rotation={[0,0,Math.PI]} >
+            <extrudeBufferGeometry args={[blocks[0], extrudeSettings]}/>            
+            <meshBasicMaterial map={texture??null} color={col??"crimson"}/>
         </mesh> 
+        <mesh scale={sscale} rotation={[0,0,Math.PI]} >
+            <extrudeBufferGeometry args={[blocks[1], extrudeSettings]}/>            
+            <meshBasicMaterial map={texture??null} color={col??"crimson"}/>
+        </mesh> 
+        <mesh scale={sscale} rotation={[0,0,Math.PI]} >
+            <extrudeBufferGeometry args={[blocks[2], extrudeSettings]}/>            
+            <meshBasicMaterial map={texture??null} color={col??"crimson"}/>
+        </mesh>  */}
+        </group>
+
         </>
      );
 }
