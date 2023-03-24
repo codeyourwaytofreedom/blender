5c1351e222d3953f10ce29b403e2b270855558f8
codeyourwaytofreedom <can.kolej@gmail.com>
2023-03-24 20:23:10 +0300

    vercel retest

--- components/home.tsx
+++ components/home.tsx
@@ -1,19 +1,9 @@
-import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { NextPage } from "next";
-import { GridHelper } from "three";
 import h from "../styles/home.module.css";
-import ARGE from "./arge";
-import Arrows from "./arrows";
-import Butterfly1 from "./butterfly1";
-import Butterfly_flying from "./Butterfly_flying";
 import Cube from "./Cube";
-import MyComponent from "./Instances";
 import Sphere from "./Sphere";
 import Cylinder from "./Cylinder";
 import { Suspense } from "react";
-import { useEffect, useRef } from "react";
-import { useState } from "react";
 import { io, Socket } from "socket.io-client";
 
 
