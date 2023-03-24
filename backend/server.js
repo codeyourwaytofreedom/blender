e6413db31e66dad8f2f4fba3aa14b05e26e71611
codeyourwaytofreedom <can.kolej@gmail.com>
2023-03-24 20:48:09 +0300

    tetsing serverjs

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,16 @@ const cors = require('cors');
 
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server, {
+/* const io = new Server(server, {
     cors: {
       origin: "http://localhost:3000",
+      methods: ["GET", "POST"]
+    }
+  }); */
+
+  const io = new Server(server, {
+    cors: {
+      origin: "https://blender-codeyourwaytofreedom.vercel.app",
       methods: ["GET", "POST"]
     }
   });
